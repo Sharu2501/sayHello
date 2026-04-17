e55f1d6fd3e003dc8384e50c9cad099de2228daa
Sharu2501 <Shakana2604@gmail.com>
2026-04-17 10:40:24 +0200

    Tests avec jacoco

--- src/main/java/org/example/sayhello/HelloService.java
+++ src/main/java/org/example/sayhello/HelloService.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.*;
 public class HelloService {
     @GetMapping("/")
     public String hello() {
-        return "hello sharu";
+        return "hello";
     }
 }
